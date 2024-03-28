26dd7544d286d116e187ba84549ae8afdfabd25f
modularbox <158810916+modularbox@users.noreply.github.com>
2024-03-28 22:47:44 +0100

    Update navbar_login.dart

--- lib/components/navbar_login.dart
+++ lib/components/navbar_login.dart
@@ -54,12 +54,16 @@ class NavBarLogin extends StatelessWidget {
         Positioned(
           bottom: 90,
           left: 75,
-          child: SizedBox(
+          child: Container(
             width: MediaQuery.of(context).size.width - (155),
-            child: Text(
-              'Premio a la Creación en la Industria del\nSoftware para la Provincia de Cáceres',
-              textAlign: TextAlign.center,
-              style: FlutterFlowTheme.of(context).labelSmall,
+            constraints: BoxConstraints(maxWidth: 300),
+            child: FittedBox(
+              fit: BoxFit.cover,
+              child: Text(
+                'Premio a la Creación en la Industria del\nSoftware para la Provincia de Cáceres',
+                textAlign: TextAlign.center,
+                style: FlutterFlowTheme.of(context).labelSmall,
+              ),
             ),
           ),
         ),
